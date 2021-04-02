fd2bc10c82739063f7ab88abb11f95921778164b
Kamil <kamil.gluza@gmail.com>
2021-04-02 21:56:23 +0200

    WIP working on function which sends test message to the server via socket

--- client/video_chat/src/App.js
+++ client/video_chat/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import LeftPanel from './LeftPanel/LeftPanel'
 import RightPanel from './RightPanel/RightPanel'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,30 +6,59 @@ import Container from 'react-bootstrap/Container'
 
 const App = () => {
 
+  const io = require('socket.io-client');
+  const socket = io('127.0.0.1:8000', { autoConnect: false });
+
+  console.log("called");
+  
   const [videoOn, setVideoOn] = useState(true);
+  const [sendOn, setSendOn] = useState(false);
 
   useEffect(() => {
-    console.log("Soc called");
-    const io = require("socket.io-client");
-    const socket = io("127.0.0.1:8000", { autoConnect: false });
-    //socket.connect();
-    socket.on("connect", () => {
-        console.log(socket.id);
+    console.log('SoC called');
+
+    socket.connect();
+    socket.on('connect', () => {
+      console.log(socket.id);
     });
+
+    socket.on('connect_error', () => {
+      
+    });
+
+    socket.on('connect_failed', () => {
+      
+    });
+
+    socket.on('disconnect', () => {
+      
+    });
+
   }, []);
 
-    const onButtonClickHandler = () => {
-      setVideoOn(!videoOn);
-    }
+  const onButtonClickHandler = () => {
+    setVideoOn(!videoOn);
+  };
 
-    return(
-      <Container fluid >
-        <div className = 'row vh-100'>
-          <LeftPanel />
-          <RightPanel onClick={onButtonClickHandler} videoOn={videoOn}/>
-        </div>
-      </Container>
-    );
+  const onButtonSendClickhandler = () => {
+    setSendOn(!sendOn);
+    const interval = () => {
+        if (sendOn) {
+          interval = setInterval(() => {
+            console.log("Hej");
+          }, 1000);
+        }
+    };
+  };
+
+  return(
+    <Container fluid >
+      <div className = 'row vh-100'>
+        <LeftPanel onClick={onButtonSendClickhandler} sendOn={sendOn} />
+        <RightPanel onClick={onButtonClickHandler} videoOn={videoOn}/>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
--- client/video_chat/src/LeftPanel/LeftPanel.js
+++ client/video_chat/src/LeftPanel/LeftPanel.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import User from './User/User'
 import classes from './LeftPanel.module.css'
+import Button from 'react-bootstrap/Button'
 
-const LeftPanel = () => {
+const LeftPanel = (props) => {
     const style = `${classes.LeftPanel} col-md-4`
     return (
         <div className={style}>
             <User name="Kamil" status={true}></User>
             <User name="Adam" status={true}></User>
             <User name="Łukasz" status={true}></User>
+            <center><Button onClick={props.onClick}>Send</Button></center>
+            { props.sendOn? 
+            <p>Sending...</p> 
+            : null }
         </div>
     );
 };
