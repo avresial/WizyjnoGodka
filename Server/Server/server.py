b6d3cf8922647705690cfe51762c0d13650afcc7
Krupenz <krupenz97@gmail.com>
2021-04-09 22:01:24 +0200

    added simple invitation event + some refactor

--- Server/Server/server.py
+++ Server/Server/server.py
@@ -4,7 +4,7 @@ from User import user_list
 
 
 ROOM = 'room'
-list = user_list.UserList()
+users_list = user_list.UserList()
 sio = socketio.AsyncServer(cors_allowed_origins='*')
 app = web.Application()
 sio.attach(app)
@@ -13,21 +13,22 @@ sio.attach(app)
 @sio.event
 async def connect(sid, environ):
     print('Connected', sid)
-    sio.enter_room(sid, ROOM)
-    list.append_new_user(sid)
+    users_list.append_new_user(sid)
+    sio.leave_room(sid, str(sid))
 
 
 @sio.event
 async def disconnect(sid):
-    list.remove_user(sid)
+    users_list.remove_user(sid)
     await sio.emit('remove-connection', data=sid, skip_sid=sid)
-    sio.leave_room(sid, ROOM)
+    for room in sio.rooms(sid):
+        sio.leave_room(sid, room)
     print('Disconnected', sid)
 
 
 @sio.on('connections')
 async def get_users(sid):
-    str = list.get_user_list_in_json(sid)
+    str = users_list.get_user_list_in_json(sid)
     await sio.emit('connections', data=str, to=sid)
 
 
@@ -44,13 +45,36 @@ async def pass_data(sid, message):
 
 @sio.on('name')
 async def pass_name(sid, name):
-    for user in list.list_of_users:
+    for user in users_list.list_of_users:
         if user.compare(sid):
             user.set_name(name)
-            json_object = list.parse_user_to_json(user)
+            json_object = users_list.parse_user_to_json(user)
             await sio.emit('add-connection', data=json_object, skip_sid=sid)
             break
 
 
+@sio.on('send-invitation')
+async def send_invitation(sid, invited_sid):
+    await sio.emit('receive-invite', data=sid, to=invited_sid)
+
+
+@sio.on('accept-invitation')
+async def accept_invitation(invited_sid, invitation_sender_sid, data):
+    if data == 'Accepted':
+        remove_from_all_rooms(invitation_sender_sid)
+        sio.enter_room(invitation_sender_sid, f"{invitation_sender_sid}-room")
+        remove_from_all_rooms(invited_sid)
+        sio.enter_room(invited_sid, f"{invitation_sender_sid}-room")
+    else:
+        await sio.emit('log', data='', to=invited_sid)
+
+
+def remove_from_all_rooms(sid):
+    for room in sio.rooms(sid):
+        sio.leave_room(sid, room)
+
+
 def start_server():
     web.run_app(app, port=8000)
+
+
