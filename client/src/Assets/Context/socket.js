aadb4f1d80422c7a4ee1577374609ba1f61b7ea1
MatysZeZ <casyneu@gmail.com>
2022-01-12 20:12:11 +0000

    Added IP_ADDRES env var for frontend and docker-compose file

--- client/src/Assets/Context/socket.js
+++ client/src/Assets/Context/socket.js
@@ -1,5 +1,7 @@
+const IP_ADDRES = process.env.IP_ADDRES || '127.0.0.1';
+const PORT = process.env.PORT || '8000'
 export const io = require('socket.io-client');
-export const socket = io('127.0.0.1:8000', { autoConnect: false });
+export const socket = io( IP_ADDRES + ':' + PORT, { autoConnect: false });
 export const PC_CONFIG = {
         iceServers: [
         {
